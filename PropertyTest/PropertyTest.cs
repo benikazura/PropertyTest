6036fa1f3a97e7f1640775d42c71abedabeec3e3
Naoki Okamoto <mtok1365tktk@outlook.jp>
2024-01-07 00:03:57 +0900

    プロジェクト ファイルを追加します。

---
+++ PropertyTest/PropertyTest.cs
@@ -0,0 +1,34 @@
+﻿using PropertyTest.CustomProperty;
+using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace PropertyTest
+{
+    [VideoEffect("カスタムプロパティテスト", new[] { "テスト" }, new string[] { })]
+    internal class PropertyTest : VideoEffectBase
+    {
+        public override string Label => "カスタムプロパティテスト";
+
+        
+        [Display(GroupName = "カスタムプロパティテスト", Name = "カスタムコントロール", Description = "カスタムコントロール")]
+        [IncreaseDecreaseButton]
+        public int IntValue { get => intValue; set => Set(ref intValue, value); }
+        int intValue = 0;
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return Enumerable.Empty<string>();
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new PropertyTestProcessor(this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => Enumerable.Empty<IAnimatable>();
+    }
+}
---
+++ PropertyTest/PropertyTestProcessor.cs
@@ -0,0 +1,45 @@
+﻿using Vortice.Direct2D1;
+using YukkuriMovieMaker.Player.Video;
+
+namespace PropertyTest
+{
+    internal class PropertyTestProcessor : IVideoEffectProcessor
+    {
+        readonly PropertyTest item;
+        ID2D1Image? input;
+
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + "is null");
+
+        public PropertyTestProcessor(PropertyTest item)
+        {
+            this.item = item;
+        }
+
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            return new DrawDescription(
+                effectDescription.DrawDescription,
+                draw: new System.Numerics.Vector3(
+                    effectDescription.DrawDescription.Draw.X + item.IntValue * 100,
+                    effectDescription.DrawDescription.Draw.Y,
+                    effectDescription.DrawDescription.Draw.Z));
+        }
+        public void ClearInput()
+        {
+            input = null;
+        }
+        public void SetInput(ID2D1Image input)
+        {
+            this.input = input;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
